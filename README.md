6d6af79486e04778e85116bebe8f41c7407897d9
Chris Prather <chris@prather.org>
2012-02-18 19:24:40 -0500

    initial import from agaton

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bot::Pip
+
+The source for the `mekano-pip` bot I have on various networks.
+
---
+++ bin/pip.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/env perl
+use lib qw(lib);
+use Bot::Pip;
+Bot::Pip->new_with_options()->run;
